859dfa8dbcc6f863f21ed187a5c30ac0bc552e4c
jasonrahm <citizenelah@gmail.com>
2021-12-01 17:27:19 -0600

    Day 1 complete.

---
+++ day1_sonar_sweep_part1.py
@@ -0,0 +1,15 @@
+"""
+Advent of Code - Day 1
+Puzzle: Sonar Sweep Part 1
+"""
+with open('input/sonar_sweep_data.txt', 'r') as f:
+    depths: list = [int(x) for x in f.readlines()]
+
+count: int = 0
+for index, value in enumerate(depths):
+    try:
+        if depths[index + 1] > depths[index]:
+            count += 1
+    except IndexError:
+        pass
+print(count)
---
+++ day1_sonar_sweep_part2.py
@@ -0,0 +1,20 @@
+"""
+Advent of Code - Day 1
+Puzzle: Sonar Sweep Part 2
+"""
+with open('input/sonar_sweep_data.txt', 'r') as f:
+    depths: list = [int(x) for x in f.readlines()]
+
+count: int = 0
+for index, value in enumerate(depths):
+    try:
+        if depths[index + 1] + \
+           depths[index + 2] + \
+           depths[index + 3] > \
+           depths[index] + \
+           depths[index + 1] + \
+           depths[index + 2]:
+            count += 1
+    except IndexError:
+        pass
+print(count)
